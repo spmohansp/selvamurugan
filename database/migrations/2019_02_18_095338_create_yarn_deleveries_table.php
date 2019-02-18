9faf9fc737666720c92a5e82de3fa4ed16c57a79
spmohansp <spmohansp@gmail.com>
2019-02-18 15:38:35 +0530

    migration delevery added

---
+++ database/migrations/2019_02_18_095338_create_yarn_deleveries_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateYarnDeleveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('yarn_deleveries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('unit_id')->unsigned();
+            $table->foreign('unit_id')->references('id')->on('units');
+            $table->integer('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->integer('sub_customer_id')->unsigned()->nullable();
+            $table->foreign('sub_customer_id')->references('id')->on('sub_customers');
+            $table->string('date')->nullable();
+            $table->integer('company_id')->unsigned()->nullable();
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->string('total_bag')->nullable();
+            $table->string('total_kg_bag')->nullable();
+            $table->string('net_weight')->nullable();
+            $table->string('color')->nullable();
+            $table->string('yarn_count')->nullable();
+            $table->string('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yarn_deleveries');
+    }
+}
